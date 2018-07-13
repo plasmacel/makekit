7cf561ea9df8f0d144005a62614c86afb83ae5d7
Celestin de Villa <plasmacel@gmail.com>
2018-07-13 22:44:09 +0200

    Added macOS installer

---
+++ macOS/install.sh
@@ -0,0 +1,31 @@
+echo Detecting Homebrew...
+
+which -s brew
+if [[ $? != 0 ]] ; then
+	# Install Homebrew
+	echo Homebrew not found, installing...
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+	# Update Homebrew
+	brew -v
+	echo Homebrew found, updating...
+	brew update
+	brew upgrade
+fi
+
+echo Detecting LLVM...
+
+brew ls --versions llvm
+if [[ $? != 0 ]] ; then
+	# Install LLVM
+	echo LLVM not found, installing...
+	brew install --with-toolchain llvm
+
+	echo Adding LLVM binaries to PATH...
+	echo 'export PATH="/usr/local/opt/llvm/bin:$PATH"' >> ~/.bash_profile
+else
+	# Update LLVM
+	brew info llvm
+	echo LLVM found, updating...
+	brew upgrade llvm
+fi
