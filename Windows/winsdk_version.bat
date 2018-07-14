10edbab448354532f3c499d1431dac37fdf07356
Celestin de Villa <plasmacel@gmail.com>
2018-07-14 23:06:48 +0200

    Update winsdk_version.bat

--- Windows/winsdk_version.bat
+++ Windows/winsdk_version.bat
@@ -1,15 +1,17 @@
 @echo off
 
-REM Set Windows Kits directory
+:: Set Windows Kits directory
 set WINSDK_DIR=C:\Program Files (x86)\Windows Kits\10\Include
 
-REM Search last directory in lexicographical order
+:: Search last directory in lexicographical order
 cd %WINSDK_DIR%
 setlocal enableDelayedExpansion
 for /f %%G in ('dir /b') do set WINSDK_VER=%%~G
 
-REM Print value
+:: Print value
 echo Detected Windows 10 SDK Version: %WINSDK_VER%
 
-REM Return value
+:: Return value
 endlocal&set %~1=%WINSDK_VER%
+
+@echo on
