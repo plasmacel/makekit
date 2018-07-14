61dc867308df719096dd8376aad3baf5573ef09f
Celestin de Villa <plasmacel@gmail.com>
2018-07-14 23:37:56 +0200

    Update mk_build.bat

--- Windows/mk_build.bat
+++ Windows/mk_build.bat
@@ -1,7 +1,11 @@
 @echo off
 
 :: Set environment variables for x64
-call vcvars64.bat
+if not "%VSCMD_ARG_TGT_ARCH%" == "x64" (
+	if not "%VSCMD_ARG_HOST_ARCH%" == "x64" (
+		call vcvars64.bat
+	)
+)
 
 :: Run Ninja
 ninja -v -C build
