7d6ed7f44f76bf83d1f93f61bbaa550ad4f6b82d
Celestin de Villa <plasmacel@gmail.com>
2018-07-16 00:05:27 +0200

    Update mk_config.bat

--- Windows/mk_config.bat
+++ Windows/mk_config.bat
@@ -15,10 +15,7 @@ if "%1" == "debug" (
 cd %~dp0
 
 :: Delete build directory if exists
-if exist "build_%1\" (
-	echo "Deleting previous build directory..."
-	@RD /S /Q "build_%1"
-)
+call mk_clean.bat
 
 :: Set environment variables for x64
 if not "%VSCMD_ARG_TGT_ARCH%" == "x64" (
@@ -30,7 +27,6 @@ if not "%VSCMD_ARG_TGT_ARCH%" == "x64" (
 :: Run CMake
 echo Configuring %1 build...
 cmake %~dp0 -G "Ninja" -Bbuild_%1 -DCMAKE_C_COMPILER:PATH="clang-cl.exe" -DCMAKE_CXX_COMPILER:PATH="clang-cl.exe" -DCMAKE_LINKER:PATH="lld-link.exe" -DCMAKE_RC_COMPILER:PATH="rc.exe" -DCMAKE_BUILD_TYPE="%BUILD_TYPE%"
-:: cmake %~dp0 -G "Ninja" -Bbuild_%1 -DCMAKE_C_COMPILER="%MAKEKIT_LLVM_BIN%/clang-cl.exe" -DCMAKE_CXX_COMPILER="%MAKEKIT_LLVM_BIN%/clang-cl.exe" -DCMAKE_LINKER="%MAKEKIT_LLVM_BIN%/lld-link.exe" -DCMAKE_BUILD_TYPE="%BUILD_TYPE%"
 :: cmake %~dp0 -G "Ninja" -Bbuild -DCMAKE_TOOLCHAIN_FILE="MakeKitToolchain.cmake" -DCMAKE_BUILD_TYPE="%BUILD_TYPE%"
 
 @echo on
