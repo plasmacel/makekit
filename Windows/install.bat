f84f62bc647880e7df5f924a71294e7419c98452
Celestin de Villa <plasmacel@gmail.com>
2018-07-16 10:53:27 +0200

    Update install.bat

--- Windows/install.bat
+++ Windows/install.bat
@@ -1,11 +1,20 @@
 @echo off
-call %~dp0\winsdk_version.bat WINSDK_VER
 
 set DEFAULT_CMAKE_INSTALL=C:\Program Files\CMake
 set DEFAULT_LLVM_INSTALL=C:\Program Files\LLVM
+set DEFAULT_MAKEKIT_INSTALL=C:\Program Files\MakeKit
 
-REM set /p WINSDK_VER=Windows SDK Version (default is %WINSDK_VER%):
-set /p CMAKE_INSTALL=LLVM installation directory (default is %DEFAULT_CMAKE_INSTALL%):
+:: Get latest installed WIndows 10 SDK version
+
+call %~dp0\winsdk_version.bat WINSDK_VER
+
+:: Get installation directories from user input
+
+:: set /p WINSDK_VER=Windows SDK Version (default is %WINSDK_VER%):
+set /p CMAKE_INSTALL=CMake installation directory (default is %DEFAULT_CMAKE_INSTALL%):
+if "%CMAKE_INSTALL%" == "" (
+	set CMAKE_INSTALL=%DEFAULT_CMAKE_INSTALL%
+)
 if not exist "%CMAKE_INSTALL%" (
 	echo ERROR: CMake installation directory cannot be found!
 	set /p dummy=Press ENTER...
@@ -14,6 +23,9 @@ if not exist "%CMAKE_INSTALL%" (
 )
 
 set /p LLVM_INSTALL=LLVM installation directory (default is %DEFAULT_LLVM_INSTALL%):
+if "%LLVM_INSTALL%" == "" (
+	set LLVM_INSTALL=%DEFAULT_LLVM_INSTALL%
+)
 if not exist "%LLVM_INSTALL%" (
 	echo ERROR: LLVM installation directory cannot be found!
 	set /p dummy=Press ENTER...
@@ -21,28 +33,45 @@ if not exist "%LLVM_INSTALL%" (
 	exit
 )
 
-REM Set dependency path variables
+set /p MAKEKIT_INSTALL=MakeKit installation directory (default is %DEFAULT_MAKEKIT_INSTALL%):
+if "%MAKEKIT_INSTALL%" == "" (
+	set MAKEKIT_INSTALL=%DEFAULT_MAKEKIT_INSTALL%
+)
+if not exist "%MAKEKIT_INSTALL%" (
+	mkdir "%MAKEKIT_INSTALL%\bin"
+)
+
+:: Set dependency path variables
+
 set VCVARS_DIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
 set WINSDK_DIR=C:\Program Files (x86)\Windows Kits\10\bin\%WINSDK_VER%\x64
 set CMAKE_BIN=%CMAKE_INSTALL%\bin
 set LLVM_BIN=%LLVM_INSTALL%\bin
+set MAKEKIT_BIN=%MAKEKIT_INSTALL%\bin
 
-echo Installing Ninja...
+:: Set environment variables
 
-copy "%~dp0\ninja\ninja.exe" "%CMAKE_BIN%\ninja.exe"
-
-echo Adding required environment variables...
+echo Setting required environment variables...
 
 setx MAKEKIT_CMAKE_BIN "%CMAKE_BIN:\=/%"
 setx MAKEKIT_LLVM_BIN "%LLVM_BIN:\=/%"
-
-echo Adding required dependencies to user PATH...
 
 PowerShell -NoProfile -ExecutionPolicy Bypass -file "%~dp0\addpath.ps1" "%VCVARS_DIR%"
 PowerShell -NoProfile -ExecutionPolicy Bypass -file "%~dp0\addpath.ps1" "%WINSDK_DIR%"
 
 PowerShell -NoProfile -ExecutionPolicy Bypass -file "%~dp0\addpath.ps1" "%CMAKE_BIN%"
 PowerShell -NoProfile -ExecutionPolicy Bypass -file "%~dp0\addpath.ps1" "%LLVM_BIN%"
+PowerShell -NoProfile -ExecutionPolicy Bypass -file "%~dp0\addpath.ps1" "%MAKEKIT_BIN%"
+
+:: Copying files
+
+echo Copying files to %MAKEKIT_BIN%...
+copy "%~dp0\ninja\ninja.exe" "%MAKEKIT_BIN%\ninja.exe"
+copy "%~dp0\mk.bat" "%MAKEKIT_BIN%\mk.bat"
+copy "%~dp0\mk_clean.bat" "%MAKEKIT_BIN%\mk_clean.bat"
+copy "%~dp0\mk_config.bat" "%MAKEKIT_BIN%\mk_config.bat"
+copy "%~dp0\mk_make.bat" "%MAKEKIT_BIN%\mk_make.bat"
+copy "%~dp0\mk_remake.bat" "%MAKEKIT_BIN%\mk_remake.bat"
 
 echo Done.
 set /p dummy=Press ENTER...
