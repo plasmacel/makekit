6f9dcfc4bc454cf1639e92bf39b27c86306bee6c
Celestin de Villa <plasmacel@gmail.com>
2018-07-16 16:49:37 +0200

    Update mk.bat

--- Windows/mk.bat
+++ Windows/mk.bat
@@ -6,6 +6,10 @@ if "%1" == "clean" (
 	call mk_config.bat %2
 ) else if "%1" == "make" (
 	call mk_make.bat %2
+) else if "%1" == "reconfig" (
+	call mk_reconfig.bat %2
+) else if "%1" == "refresh" (
+	call mk_refresh.bat %2
 ) else if "%1" == "remake" (
 	call mk_remake.bat %2
 ) else (
