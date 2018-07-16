55cbe14da4c5df64aca1ebcd051da80d4cf79127
Celestin de Villa <plasmacel@gmail.com>
2018-07-16 16:13:48 +0200

    Update mk_make.bat

--- Windows/mk_make.bat
+++ Windows/mk_make.bat
@@ -7,20 +7,9 @@ if not "%VSCMD_ARG_TGT_ARCH%" == "x64" (
 	)
 )
 
-:: Reconfig if required
-
-set MAKEKIT_CONFIG_REQUIRED=0;
-
-if not exist "build_%1\build.ninja" ( set MAKEKIT_CONFIG_REQUIRED=1 )
-if not exist "build_%1\CMakeCache.txt" ( set MAKEKIT_CONFIG_REQUIRED=1 )
-if not exist "build_%1\rules.ninja" ( set MAKEKIT_CONFIG_REQUIRED=1 )
-
-if %MAKEKIT_CONFIG_REQUIRED% == 1 (
-	call mk_config.bat %1
-)
+:: Configure
+call mk_config.bat %1
 
 :: Run Ninja
 echo Making %1 build...
 ninja -v -C build_%1
-
-@echo on
