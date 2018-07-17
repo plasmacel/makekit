a3651e2e37482cac618ab11cd590bd8405bcfd9a
Celestin de Villa <plasmacel@gmail.com>
2018-07-17 22:21:58 +0200

    Update mk.cpp

--- src/mk.cpp
+++ src/mk.cpp
@@ -4,40 +4,74 @@
 const static std::string BUILD_DIR_PREFIX = "build.";
 const static std::string DEFAULT_BUILD_TYPE = "release";
 
-struct system_command
+struct system_commands
 {
-	std::string command;
-	
-	void append(const std::string& flag)
+	std::string commands;
+
+	system_commands& operator=(const std::string& str)
 	{
-		command += ' ' + flag;
+		this->commands = str;
+		return *this;
 	}
-	
+
+	void append(const std::string& cmd)
+	{
+		std::string sep;
+
+		if (!this->commands.empty())
+		{
+			sep = " & ";
+		}
+
+		this->commands += sep + cmd;
+	}
+
 	operator const char*() const
 	{
-		return command.c_str();
+		return this->commands.c_str();
 	}
 };
 
 #ifdef _WIN32
-void set_environment(const std::string& host_arch, const std::string& target_arch)
+void add_set_environment_command(const std::string& host_arch, const std::string& target_arch, system_commands& cmd)
 {
-	std::string current_target_arch = std::getenv("VSCMD_ARG_TGT_ARCH");
-	std::string current_host_arch = std::getenv("VSCMD_ARG_HOST_ARCH");
+	std::string current_host_arch;
+	std::string current_target_arch;
+	
+	char* buf;
+	size_t n = 0;
+
+	buf = nullptr;
+	if ((_dupenv_s(&buf, &n, "VSCMD_ARG_HOST_ARCH") == 0) && (buf != nullptr))
+	{
+		current_host_arch.assign(buf, n);
+		std::free(buf);
+	}
+
+	//std::cout << current_host_arch << std::endl;
+
+	buf = nullptr;
+	if ((_dupenv_s(&buf, &n, "VSCMD_ARG_TGT_ARCH") == 0) && (buf != nullptr))
+	{
+		current_target_arch.assign(buf, n);
+		std::free(buf);
+	}
+
+	//std::cout << current_target_arch << std::endl;
 	
 	if ((current_host_arch != host_arch) || (current_target_arch != target_arch))
 	{
-		std::system("call vcvars64.bat");
+		cmd.append("call vcvars64.bat");
 	}
 }
 
-void set_environment(const std::string& arch)
+void add_set_environment_command(const std::string& arch, system_commands& cmd)
 {
-	set_environment(arch, arch);
+	add_set_environment_command(arch, arch, cmd);
 }
 #endif
 
-int config(const std::string& build_type)
+int config(const std::string& build_type, system_commands& cmd)
 {
 	std::string cmake_build_type;
 	
@@ -62,120 +96,96 @@ int config(const std::string& build_type)
 		std::cout << "ERROR Invalid build type: " << build_type << std::endl;
 		return 1;
 	}
-	
-	std::cout << "Configuring " << build_type << " build..." << std::endl;
-	
-	// Set environment variables on Windows
-	
+
+	// Compose terminal commands
+
+	// Append set environment command (required on Windows only)
+
 	#ifdef _WIN32
-	set_environment("x64");
+	add_set_environment_command("x64", cmd);
 	#endif
 	
-	// Run CMake
-	
-	system_command command{ "cmake" };
-	command.append(".");
-	command.append("-GNinja");
-	command.append("-B" + BUILD_DIR_PREFIX + build_type);
-	
-	#ifdef _WIN32
-	command.append("-DCMAKE_C_COMPILER:PATH='clang-cl.exe'");
-	command.append("-DCMAKE_CXX_COMPILER:PATH='clang-cl.exe'");
-	command.append("-DCMAKE_LINKER:PATH='lld-link.exe'");
-	command.append("-DCMAKE_RC_COMPILER:PATH='rc.exe'");
-	//command.append("-DCMAKE_ASM_COMPILER:PATH='ml64.exe'");
-	//command.append("-DCMAKE_CUDA_COMPILER:PATH='nvcc.exe'");
-	#else
-	command.append("-DCMAKE_C_COMPILER:PATH='clang'");
-	command.append("-DCMAKE_CXX_COMPILER:PATH='clang++'");
-	command.append("-DCMAKE_LINKER:PATH='lld'");
-	//command.append("-DCMAKE_ASM_COMPILER:PATH='llvm-as'");
-	//command.append("-DCMAKE_CUDA_COMPILER:PATH='nvcc'");
+	// Append run CMake command
+
+	std::string cmake_command = "cmake";
+	cmake_command += " .";
+	cmake_command += " -GNinja";
+	cmake_command += " -B" + BUILD_DIR_PREFIX + build_type;
+	#ifdef _WIN32 // Windows flags
+	cmake_command += " -DCMAKE_C_COMPILER:PATH=\"clang-cl.exe\"";
+	cmake_command += " -DCMAKE_CXX_COMPILER:PATH=\"clang-cl.exe\"";
+	cmake_command += " -DCMAKE_LINKER:PATH=\"lld-link.exe\"";
+	cmake_command += " -DCMAKE_RC_COMPILER:PATH=\"rc.exe\"";
+	//cmake_command += " -DCMAKE_ASM_COMPILER:PATH=ml64.exe";
+	//cmake_command += " -DCMAKE_CUDA_COMPILER:PATH=nvcc.exe";
+	#else // macOS, Linux flags
+	cmake_command += " -DCMAKE_C_COMPILER:PATH=\"clang\"";
+	cmake_command += " -DCMAKE_CXX_COMPILER:PATH=\"clang++\"";
+	cmake_command += " -DCMAKE_LINKER:PATH=\"lld\"";
+	//cmake_command += " -DCMAKE_ASM_COMPILER:PATH=\"llvm-as\"";
+	//cmake_command += " -DCMAKE_CUDA_COMPILER:PATH=\"nvcc\"";
 	#endif
-	
-	command.append("-DCMAKE_BUILD_TYPE=" + cmake_build_type);
-	
-	std::system(command);
+	cmake_command += " -DCMAKE_BUILD_TYPE=" + cmake_build_type;
+
+	cmd.append(cmake_command);
 	
 	return 0;
 }
 
-int make(const std::string& build_type)
+int make(const std::string& build_type, system_commands& cmd)
 {
 	// Config or refresh
 	
-	if (config(build_type) != 0) return 1;
-	
-	// Make
-	
-	std::cout << "Making " << build_type << " build..." << std::endl;
+	if (config(build_type, cmd) != 0) return 1;
 
-	#ifdef _WIN32
-	set_environment("x64");
-	#endif
-	
-	system_command command{ "ninja" };
-	//command.append("-v");
-	command.append("-C " + BUILD_DIR_PREFIX + build_type);
-	
-	std::system(command);
-	
+	// Add Ninja build command
+
+	cmd.append("ninja -C " + BUILD_DIR_PREFIX + build_type);
+
 	return 0;
 }
 
-int clean_all(const std::string& build_type)
+int clean_all(const std::string& build_type, system_commands& cmd)
 {
 	#ifdef _WIN32
-	system_command command{ "@if exist " + BUILD_DIR_PREFIX + build_type };
-	command.append("@rd /s /q " + BUILD_DIR_PREFIX + build_type);
+	cmd.append("@if exist " + BUILD_DIR_PREFIX + build_type + " @rd /s /q " + BUILD_DIR_PREFIX + build_type);
 	#else
-	system_command command{ "rm -r -f" };
-	command.append(BUILD_DIR_PREFIX + build_type);
+	cmd.append("rm -r -f " + BUILD_DIR_PREFIX + build_type);
 	#endif
-	
-	std::system(command);
 	return 0;
 }
 
-int clean_config(const std::string& build_type)
+int clean_config(const std::string& build_type, system_commands& cmd)
 {
 	#ifdef _WIN32
-	system_command command{ "@if exist " + BUILD_DIR_PREFIX + build_type + "\CMakeCache.txt" };
-	command.append("@del /f /q " + BUILD_DIR_PREFIX + build_type + "\CMakeCache.txt");
+	cmd.append("@if exist " + BUILD_DIR_PREFIX + build_type + "\\CMakeCache.txt" + " @del /f /q " + BUILD_DIR_PREFIX + build_type + "\\CMakeCache.txt");
 	#else
-	system_command command{ "rm -f" };
-	command.append(BUILD_DIR_PREFIX + build_type + "/CMakeCache.txt");
+	cmd.append("rm -f " + BUILD_DIR_PREFIX + build_type + "/CMakeCache.txt");
 	#endif
-	
-	std::system(command);
 	return 0;
 }
 
-int clean_make(const std::string& build_type)
+int clean_make(const std::string& build_type, system_commands& cmd)
 {
-	system_command command{ "ninja" };
-	command.append("-C " + BUILD_DIR_PREFIX + build_type);
-	command.append("-t clean");
-	
-	std::system(command);
+	cmd.append("ninja -C " + BUILD_DIR_PREFIX + build_type + " -t clean");
 	return 0;
 }
 
-int refresh(const std::string& build_type)
+int refresh(const std::string& build_type, system_commands& cmd)
 {
-	return config(build_type);
+	return config(build_type, cmd);
 }
 
-int reconfig(const std::string& build_type)
+int reconfig(const std::string& build_type, system_commands& cmd)
 {
-	clean_config(build_type);
-	return config(build_type);
+	if (clean_config(build_type, cmd) != 0) return 1;
+	return config(build_type, cmd);
 }
 
-int remake(const std::string& build_type)
+int remake(const std::string& build_type, system_commands& cmd)
 {
-	clean_make(build_type);
-	return make(build_type);
+	if (clean_make(build_type, cmd) != 0) return 1;
+	return make(build_type, cmd);
 }
 
 int main(int argc, char** argv)
@@ -188,35 +198,51 @@ int main(int argc, char** argv)
 	
 	if (!command.empty() && build_type.empty()) build_type = DEFAULT_BUILD_TYPE;
 	
+	system_commands cmd;
+	int retval;
+
 	if (command == "clean")
 	{
-		return clean_all(build_type);
+		retval = clean_all(build_type, cmd);
+		if (retval != 0) return retval;
+		std::cout << "Cleaning " << build_type << " build..." << std::endl;
 	}
 	else if (command == "config")
 	{
-		return config(build_type);
+		retval = config(build_type, cmd);
+		if (retval != 0) return retval;
+		std::cout << "Configuring " << build_type << " build..." << std::endl;
 	}
 	else if (command == "make")
 	{
-		return make(build_type);
+		retval = make(build_type, cmd);
+		if (retval != 0) return retval;
+		std::cout << "Making " << build_type << " build..." << std::endl;
 	}
 	else if (command == "reconfig")
 	{
-		return reconfig(build_type);
+		retval = reconfig(build_type, cmd);
+		if (retval != 0) return retval;
+		std::cout << "Reconfiguring " << build_type << " build..." << std::endl;
 	}
 	else if (command == "refresh")
 	{
-		return refresh(build_type);
+		retval = refresh(build_type, cmd);
+		if (retval != 0) return retval;
+		std::cout << "Refresh " << build_type << " build..." << std::endl;
 	}
 	else if (command == "remake")
 	{
-		return remake(build_type);
+		retval = remake(build_type, cmd);
+		if (retval != 0) return retval;
+		std::cout << "Remaking " << build_type << " build..." << std::endl;
 	}
 	else
 	{
 		if (command.empty())
 		{
 			std::cout << "No command specified." << std::endl;
+			return 0;
 		}
 		else
 		{
@@ -224,6 +250,8 @@ int main(int argc, char** argv)
 			return 1;
 		}
 	}
+
+	std::system(cmd);
 	
 	return 0;
 }
