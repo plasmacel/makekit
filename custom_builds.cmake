c02ed6518a2220b9ad0b0920fac5c7a194bb4a18
Celestin de Villa <plasmacel@gmail.com>
2018-07-23 14:15:50 +0200

    Update custom_builds.cmake

--- custom_builds.cmake
+++ custom_builds.cmake
@@ -8,31 +8,33 @@ message("Adding custom build types...")
 
 # MYCUSTOM : Rename MYCUSTOM to anything but the protected names!
 
-set(CMAKE_CXX_FLAGS_MYCUSTOM
-    "${CMAKE_CXX_FLAGS_DEBUG} -Wpedantic"
-    STRING "Flags used by the C++ compiler during MYCUSTOM builds."
-)
+set(CMAKE_CXX_FLAGS_MYCUSTOM "${CMAKE_CXX_FLAGS_DEBUG} -Wall -Wpedantic"
+    CACHE STRING "Flags used by the C++ compiler during MYCUSTOM builds."
+    FORCE)
 
-set(CMAKE_C_FLAGS_MYCUSTOM
-    "${CMAKE_C_FLAGS_DEBUG} -Wpedantic"
-    STRING "Flags used by the C compiler during MYCUSTOM builds."
-)
+set(CMAKE_C_FLAGS_MYCUSTOM "${CMAKE_C_FLAGS_DEBUG} -Wall -Wpedantic"
+    CACHE STRING "Flags used by the C compiler during MYCUSTOM builds."
+    FORCE)
 
 set(CMAKE_EXE_LINKER_FLAGS_MYCUSTOM
     ""
-    STRING "Flags used for linking binaries during MYCUSTOM builds."
-)
+    CACHE STRING "Flags used for linking binaries during MYCUSTOM builds."
+    FORCE)
 
 set(CMAKE_SHARED_LINKER_FLAGS_MYCUSTOM
     ""
-    STRING "Flags used by the shared libraries linker during MYCUSTOM builds."
-)
+    CACHE STRING "Flags used by the shared libraries linker during MYCUSTOM builds."
+    FORCE)
 
 mark_as_advanced(
     CMAKE_CXX_FLAGS_MYCUSTOM
     CMAKE_C_FLAGS_MYCUSTOM
     CMAKE_EXE_LINKER_FLAGS_MYCUSTOM
-    CMAKE_SHARED_LINKER_FLAGS_MYCUSTOM
-)
+    CMAKE_SHARED_LINKER_FLAGS_MYCUSTOM)
+
+# Update the documentation string of CMAKE_BUILD_TYPE for GUIs
+set(CMAKE_BUILD_TYPE "${CMAKE_BUILD_TYPE}"
+    CACHE STRING "Choose the type of build, options are: None Debug Release RelWithDebInfo MinSizeRel MYCUSTOM."
+    FORCE)
 
 # Add further build types here...
