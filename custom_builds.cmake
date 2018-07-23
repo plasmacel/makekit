3789ee79a5a658209f91e1561c06c13e0219a03f
Celestin de Villa <plasmacel@gmail.com>
2018-07-23 13:41:17 +0200

    Update custom_builds.cmake

--- custom_builds.cmake
+++ custom_builds.cmake
@@ -1,7 +1,7 @@
 # MakeKit CMake file for custom build types
 # Keep in mind, that CMake BUILD_TYPEs are case-insensitive, thus
 # it doesn't make difference between lowercase and uppercase characters.
-# The following build type names are protected by CMake: None, Debug, Release, RelWithDebInfo, MinSizeRel
+# The following build type names are protected by CMake: None, Debug, Release, RelWithDebInfo, MinSizeRel.
 # You can inherit from another build types by listing their flags variable (like ${CMAKE_CXX_FLAGS_DEBUG}) in your custom flags.
 
 message("Adding custom build types...")
