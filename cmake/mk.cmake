c4041b4498e1b8d6fc06663a6cb1be2d2c8165b5
Celestin de Villa <plasmacel@gmail.com>
2018-07-26 01:42:23 +0200

    Update mk.cmake

--- cmake/mk.cmake
+++ cmake/mk.cmake
@@ -41,8 +41,8 @@ set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 # Language standard
 #
 
-set(CMAKE_C_STANDARD 11)
-set(CMAKE_CXX_STANDARD 17)
+#set(CMAKE_C_STANDARD 11)
+#set(CMAKE_CXX_STANDARD 17)
 
 #
 # OS Platform Detection
