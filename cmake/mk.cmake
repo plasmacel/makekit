3463287e5bc2a87d02f8b023d03ff83c641ab454
Celestin de Villa <plasmacel@gmail.com>
2018-07-27 01:54:24 +0200

    Update mk.cmake

--- cmake/mk.cmake
+++ cmake/mk.cmake
@@ -1,4 +1,8 @@
 cmake_minimum_required(VERSION 3.10 FATAL_ERROR)
+if (NOT CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+	message(FATAL_ERROR "MakeKit - Not a valid LLVM/clang compiler!
+		You are maybe using Apple's fork of LLVM/clang shipped with Xcode instead of the genuine one.")
+endif ()
 
 enable_language(C)
 enable_language(CXX)
