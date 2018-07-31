94b4683bd4ef515fb03bf7c9b2cfd70b081a05fb
Celestin de Villa <plasmacel@gmail.com>
2018-07-31 10:12:21 +0200

    Update llvm.darwin.x86_64.toolchain.cmake

--- cmake/toolchains/llvm.darwin.x86_64.toolchain.cmake
+++ cmake/toolchains/llvm.darwin.x86_64.toolchain.cmake
@@ -8,25 +8,9 @@ set(MK_TARGET_SYSTEM_NAME "Darwin")
 set(MK_TARGET_PROCESSOR_NAME "x86_64")
 set(MK_TARGET_TRIPLE x86_64-apple-darwin)
 
-set(CMAKE_C_COMPILER clang)
-set(CMAKE_CXX_COMPILER clang++)
-set(CMAKE_LINKER ld)
-
-#set(CMAKE_C_COMPILER clang)
-#set(CMAKE_CXX_COMPILER clang)
-#set(CMAKE_LINKER lld)
-
-#set(CMAKE_C_FLAGS_INIT --driver-mode=gcc ${CMAKE_C_FLAGS_INIT})
-#set(CMAKE_CXX_FLAGS_INIT --driver-mode=g++ ${CMAKE_CXX_FLAGS_INIT})
-
-#set(CMAKE_EXE_LINKER_FLAGS_INIT -flavor darwin)
-#set(CMAKE_MODULE_LINKER_FLAGS_INIT -flavor darwin)
-#set(CMAKE_SHARED_LINKER_FLAGS_INIT -flavor darwin)
-#set(CMAKE_STATIC_LINKER_FLAGS_INIT -flavor darwin)
-
 # https://cmake.org/cmake/help/latest/variable/CMAKE_FRAMEWORK_PATH.html
 
-include(toolchain.cmake)
+include(llvm.toolchain.cmake)
 
 if (NOT ${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 	message(FATAL_ERROR "Using the macOS toolchain while the target platform is not macOS!")
