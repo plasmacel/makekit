301ea7e96671583fba3ce6f0500161b1b0f229d5
Celestin de Villa <plasmacel@gmail.com>
2018-09-11 13:40:27 +0200

    mk.cpp fixed for non-Windows targets

--- src/mk.cpp
+++ src/mk.cpp
@@ -248,7 +248,10 @@ int make(system_commands& cmd, std::string config, const std::string& toolchain,
 			if (refresh(cmd, config)) return 1;
 		}
 
+		
+#ifdef _WIN32
 		add_set_environment_command(cmd, "x64");
+#endif
 	}
 
 	// Append build commands
