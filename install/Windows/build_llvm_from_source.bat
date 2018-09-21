e35e6cbe9c3f856a2019ffa549b2db9ca90c6e41
Celestin de Villa <plasmacel@gmail.com>
2018-09-21 13:03:28 +0200

    build_llvm_from_source.bat updates

--- install/Windows/build_llvm_from_source.bat
+++ install/Windows/build_llvm_from_source.bat
@@ -17,42 +17,51 @@ cd %MK_LLVM_INSTALL_DIR%
 svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
 
 :: Compiler-RT
-cd %MK_LLVM_INSTALL_DIR%/projects
+cd %MK_LLVM_INSTALL_DIR%
+cd llvm/projects
 svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
 
 :: OpenMP
-cd %MK_LLVM_INSTALL_DIR%/projects
+cd %MK_LLVM_INSTALL_DIR%
+cd llvm/projects
 svn co http://llvm.org/svn/llvm-project/openmp/trunk openmp
 
 :: clang
-cd %MK_LLVM_INSTALL_DIR%/tools
+cd %MK_LLVM_INSTALL_DIR%
+cd llvm/tools
 svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
 
 :: clang-tools-extra
-cd %MK_LLVM_INSTALL_DIR%/tools/clang/tools
+cd %MK_LLVM_INSTALL_DIR%
+cd llvm/tools/clang/tools
 svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
 
 :: LLD
-cd %MK_LLVM_INSTALL_DIR%/tools
+cd %MK_LLVM_INSTALL_DIR%
+cd llvm/tools
 svn co http://llvm.org/svn/llvm-project/lld/trunk lld
 
 :: Polly Loop Optimizer
-cd %MK_LLVM_INSTALL_DIR%/tools
+cd %MK_LLVM_INSTALL_DIR%
+cd llvm/tools
 svn co http://llvm.org/svn/llvm-project/polly/trunk polly
 
 :: libc++ and libc++abi
-:: cd %MK_LLVM_INSTALL_DIR%/projects
+:: cd %MK_LLVM_INSTALL_DIR%
+:: cd llvm/projects
 :: svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
 :: svn co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
 
 :: Test Suite
-:: cd %MK_LLVM_INSTALL_DIR%/projects
+:: cd %MK_LLVM_INSTALL_DIR%
+:: cd llvm/projects
 :: svn co http://llvm.org/svn/llvm-project/test-suite/trunk test-suite
 
 :: Build LLVM
 
 cd %MK_LLVM_INSTALL_DIR%
 mkdir build
+vsdevcmd -arch=x64 -host_arch=x64
 cmake llvm -Bbuild -GNinja -DCMAKE_C_COMPILER=clang-cl -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_LINKER=lld-link -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=directory -DLIBOMP_ARCH=x86_64 -DLIBOMP_CXXFLAGS=/D_GNU_SOURCE -DLLVM_ENABLE_ASSERTIONS=OFF -DLIBOMP_HAVE_WEAK_ATTRIBUTE=FALSE
 ninja -C build
 
