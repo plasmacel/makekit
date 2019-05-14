0d18753b6072bec62d7486e5cce91001b2885fdf
Celestin de Villa <plasmacel@gmail.com>
2019-05-14 16:30:21 +0200

    improved Windows install.bat

--- install/Windows/install.bat
+++ install/Windows/install.bat
@@ -4,8 +4,11 @@ set DEFAULT_CMAKE_DIR=%ProgramFiles%\CMake
 set DEFAULT_LLVM_DIR=%ProgramFiles%\LLVM
 set DEFAULT_MK_DIR=%ProgramFiles%\MakeKit
 
+echo MakeKit CLI Installer
+
 :: Get CMake installation directory
 
+echo.
 set /p MK_CMAKE_INSTALL_DIR=CMake installation directory (default is %DEFAULT_CMAKE_DIR%):
 if "%MK_CMAKE_INSTALL_DIR%" == "" (
 	set MK_CMAKE_INSTALL_DIR=%DEFAULT_CMAKE_DIR%
@@ -16,11 +19,19 @@ if not exist "%MK_CMAKE_INSTALL_DIR%" (
 	@echo on
 	exit /b 1
 )
+if exist "%MK_CMAKE_INSTALL_DIR%/bin/cmake.exe" (
+	echo CMake found: "%MK_CMAKE_INSTALL_DIR%\bin\cmake.exe"
+) else (
+	echo Error: CMake cannot be found at "%MK_CMAKE_INSTALL_DIR%/bin/cmake.exe"!
+	set /p dummy=Press ENTER...
+	exit /b 1
+)
 
 :: Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Kitware\CMake :: InstallDir
 
 :: Get LLVM installation directory
 
+echo.
 set /p MK_LLVM_INSTALL_DIR=LLVM installation directory (default is %DEFAULT_LLVM_DIR%):
 if "%MK_LLVM_INSTALL_DIR%" == "" (
 	set MK_LLVM_INSTALL_DIR=%DEFAULT_LLVM_DIR%
@@ -31,11 +42,19 @@ if not exist "%MK_LLVM_INSTALL_DIR%" (
 	@echo on
 	exit /b 1
 )
+if exist "%MK_LLVM_INSTALL_DIR%/bin/clang-cl.exe" (
+	echo Clang found: "%MK_LLVM_INSTALL_DIR%\bin\clang-cl.exe"
+) else (
+	echo Error: Clang cannot be found at "%MK_LLVM_INSTALL_DIR%/bin/clang-cl.exe"!
+	set /p dummy=Press ENTER...
+	exit /b 1
+)
 
 :: Computer\HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\LLVM\LLVM :: (Default)
 
 :: Get MK installation directory
 
+echo.
 set /p MK_INSTALL_DIR=MakeKit installation directory (default is %DEFAULT_MK_DIR%):
 if "%MK_INSTALL_DIR%" == "" (
 	set MK_INSTALL_DIR=%DEFAULT_MK_DIR%
@@ -49,6 +68,7 @@ if exist "%MK_INSTALL_DIR%" (
 
 :: Get Qt installation directory
 
+echo.
 set /p MK_QT_INSTALL_DIR=Qt installation directory (optional):
 if "%MK_QT_INSTALL_DIR%" == "" (
 	echo Qt support disabled.
@@ -62,66 +82,33 @@ if "%MK_QT_INSTALL_DIR%" == "" (
 :: Install Visual Studio 2017 Build Tools
 :: https://blogs.msdn.microsoft.com/vcblog/2016/11/16/introducing-the-visual-studio-build-tools/
 
-:: vs_buildtools.exe â€“quiet â€“add Microsoft.VisualStudio.Workload.VCTools â€“includeRecommended
+:: vs_buildtools.exe –quiet –add Microsoft.VisualStudio.Workload.VCTools –includeRecommended
 
 :: Set MK environment variables
-
-set MK_CMAKE_BIN="%MK_CMAKE_INSTALL_DIR%\bin"
-set MK_LLVM_BIN="%MK_LLVM_INSTALL_DIR%\bin"
-set MK_BIN="%MK_INSTALL_DIR%\bin"
 
 echo.
 echo Creating environment variable MK_DIR...
 setx MK_DIR "%MK_INSTALL_DIR:\=/%"
 
 echo.
+echo Creating environment variable MK_CMAKE...
+setx MK_CMAKE_DIR "%MK_CMAKE_INSTALL_DIR:\=/%/bin/cmake.exe"
+
+echo.
 echo Creating environment variable MK_LLVM_DIR...
 setx MK_LLVM_DIR "%MK_LLVM_INSTALL_DIR:\=/%"
+
+echo.
+echo Creating environment variable MK_NINJA...
+setx MK_CMAKE_DIR "%MK_INSTALL_DIR:\=/%/bin/ninja.exe"
 
 echo.
 echo Creating environment variable MK_QT_DIR...
 setx MK_QT_DIR "%MK_QT_INSTALL_DIR:\=/%"
 
-:: Add required directories to the PATH
-
 echo.
-echo Extending environment variable PATH...
-PowerShell -NoProfile -ExecutionPolicy Bypass -file "%~dp0\export_path.ps1" "%MK_CMAKE_BIN%"
-PowerShell -NoProfile -ExecutionPolicy Bypass -file "%~dp0\export_path.ps1" "%MK_LLVM_BIN%"
-PowerShell -NoProfile -ExecutionPolicy Bypass -file "%~dp0\export_path.ps1" "%MK_BIN%"
-
-:: Check required components
-
-echo.
-echo Checking the presence of CMake...
-where /q cmake
-if %ERRORLEVEL% == 0 (
-	echo CMake is OK!
-) else (
-	echo Error: CMake cannot be found in PATH!
-	set /p dummy=Press ENTER...
-	exit /b 1
-)
-
-echo Checking the presence of Ninja...
-where /q ninja
-if %ERRORLEVEL% == 0 (
-	echo Ninja is OK!
-) else (
-	echo Error: Ninja cannot be found in PATH!
-	set /p dummy=Press ENTER...
-	exit /b 1
-)
-
-echo Checking the presence of clang-cl...
-where /q clang-cl
-if %ERRORLEVEL% == 0 (
-	echo clang-cl is OK!
-) else (
-	echo Error: clang-cl cannot be found in PATH!
-	set /p dummy=Press ENTER...
-	exit /b 1
-)
+echo Creating environment variable MK_QT_QMLDIR...
+setx MK_QT_QMLDIR "%MK_QT_INSTALL_DIR:\=/%/qml"
 
 :: Building source
 
@@ -145,8 +132,8 @@ cd "%~dp0\..\.."
 
 ::ninja -C build
 mkdir build && mkdir build\bin
-%MK_LLVM_DIR%/bin/clang-cl /nologo /EHsc /MD /O2 /Ob2 /DNDEBUG src/mk.cpp /o build\bin\
-%MK_LLVM_DIR%/bin/clang-cl /nologo /EHsc /MD /O2 /Ob2 /DNDEBUG src/llvm-rc-rc.cpp /o build\bin\
+"%MK_LLVM_DIR%/bin/clang-cl" /nologo /EHsc /MD /O2 /Ob2 /DNDEBUG src/mk.cpp /o build\bin\
+"%MK_LLVM_DIR%/bin/clang-cl" /nologo /EHsc /MD /O2 /Ob2 /DNDEBUG src/llvm-rc-rc.cpp /o build\bin\
 
 if %ERRORLEVEL% == 0 (
 	echo Build succeeded.
